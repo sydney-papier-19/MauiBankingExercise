d5648f5e8b5d3b04d21a1e406a37b8b0dc6156ae
sydney_p <sydneypapier23@gmail.com>
2025-08-20 21:09:36 +0200

    added Views folder and CustomerSelectionViewModel code

--- MauiBankingExercise/ViewModels/CustomerSelectionViewModel.cs
+++ MauiBankingExercise/ViewModels/CustomerSelectionViewModel.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MauiBankingExercise.ViewModels
 {
-    internal class CustomerSelectionViewModel
+    public class CustomerSelectionViewModel
     {
+        private readonly SqliteDatabaseService _dbService;
+
+        public ObservableCollection<CustomerSelectionViewModel> Customers { get; set; }
+
+        public CustomerSelectionViewModel(SqliteDatabaseService dbService)
+        {
+            _dbService = dbService;
+
+            Customers = new ObservableCollection<CustomerSelectionViewModel>();
+
+            LoadCustomers();
+ 
+        }
+
+        private void LoadCustomers()
+        {
+            var connection = _dbService.GetConnection();
+
+            var customers = connection.Table<CustomerSelectionViewModel>().ToList();
+
+            Customers.Clear();
+            foreach (var customer in customers)
+            {
+                Customers.Add(customer);
+            }
+                
+        
+        }
     }
 }
